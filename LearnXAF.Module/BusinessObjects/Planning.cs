aff4f99a73ccf865966d994e9eaa329487f5d40b
tegarsweefer <tegar.s@weefer.co.id>
2019-07-24 08:34:55 +0700

    first commit

---
+++ LearnXAF.Module/BusinessObjects/Planning.cs
@@ -0,0 +1,127 @@
+﻿using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+
+namespace LearnXAF.Module.BusinessObjects.Planning
+{
+    [NavigationItem("Planning")]
+    public class Student : BaseObject
+    {
+        public Student(Session session) : base(session) { }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+		public DateTime DateofBirth { get; set; }
+        [Size(255)]
+        public String Address { get; set; }
+    }
+    [NavigationItem("Planning")]
+	[Appearance("Appearance for Teacher.DateOfBirth",TargetItems ="DateofBirth",Enabled =false,Criteria ="EnableDateOfBirth")]
+	[Appearance("Appearance for Teacher.TanggalUpdate",TargetItems = "TanggalUpdate", Enabled =false,Criteria ="TRUE")]
+    public class Teacher : BaseObject
+    {
+        public Teacher(Session session) : base(session) { }
+		[RuleRequiredField(DefaultContexts.Save)]
+        public String FirstName { get; set; }
+        [RuleRequiredField(DefaultContexts.Save)]
+        public String LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        [Size(255)]
+        public String Address { get; set; }
+		public DateTime TanggalUpdate { get; set; }
+		public int Age
+        {
+			get
+            {
+				if (this.DateofBirth != new DateTime())
+                {
+                    return DateTime.Now.Year - this.DateofBirth.Year;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        protected override void OnSaving()
+        {
+            this.TanggalUpdate = DateTime.Now;
+            base.OnSaving();
+        }
+
+        protected override void OnDeleting()
+        {
+            base.OnDeleting();
+        }
+
+        private bool EnableDateOfBirth
+        {
+			get
+            {
+				if (FirstName.Trim() == string.Empty)
+                {
+                    return false;
+                }
+				else if (FirstName.Contains("Obama"))
+                {
+                    return false;
+                }
+				else
+                {
+                    return true;
+                }
+            }
+        }
+
+		[Association("Association for Teacher.TeacherSubject"),DevExpress.Xpo.Aggregated()]
+        public XPCollection<TeacherSubject> Subjects
+        {
+			get
+            {
+                return GetCollection<TeacherSubject>("Subjects");
+            }
+        }
+    }
+    [NavigationItem("Planning")]
+    public class Subject : BaseObject
+    {
+        public Subject(Session session) : base(session) { }
+        public String Code { get; set; }
+        public String Name { get; set; }
+    }
+    [NavigationItem("Planning")]
+    public class TeacherSubject : BaseObject
+    {
+        public TeacherSubject(Session session) : base(session) { }
+
+        [Association("Association for Teacher.TeacherSubject")]
+        public Teacher Teacher { get; set; }
+        public Subject Subject { get; set; }
+    }
+
+
+    [NavigationItem("Planning")]
+    public class Class : BaseObject
+    {
+        public Class(Session session) : base(session) { }
+        public String Code { get; set; }
+        public String Name { get; set; }
+    }
+    [NavigationItem("Planning")]
+    public class ClassStudent : BaseObject
+    {
+        public ClassStudent(Session session) : base(session) { }
+        public Class Class { get; set; }
+        public Student Student { get; set; }
+    }
+    [NavigationItem("Planning")]
+    public class ClassTeacher : BaseObject
+    {
+        public ClassTeacher(Session session) : base(session) { }
+        public Class Class { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ LearnXAF.Win/WinApplication.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace LearnXAF.Win {
+    partial class LearnXAFWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new LearnXAF.Module.LearnXAFModule();
+            this.module4 = new LearnXAF.Module.Win.LearnXAFWindowsFormsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // LearnXAFWindowsFormsApplication
+            // 
+            this.ApplicationName = "LearnXAF";
+            this.LinkNewObjectToParentImmediately = false;
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.LearnXAFWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.LearnXAFWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private LearnXAF.Module.LearnXAFModule module3;
+        private LearnXAF.Module.Win.LearnXAFWindowsFormsModule module4;
+    }
+}
